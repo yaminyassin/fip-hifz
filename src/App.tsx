0a596bf53296219d75fdd0802d5ee70a6cf33a70
Yamin <Yamin2008@live.com>
2024-11-24 17:59:16 +0000

    feat: add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,58 @@
 import "./App.css";
 import { Button } from "./components/ui/button";
 import { Label } from "./components/ui/label";
+import { doc, setDoc, onSnapshot } from "firebase/firestore";
+import { firestore } from "./main";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const docId = "counterDoc"; // Define a constant document ID
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const docRef = doc(firestore, "count", docId);
+        await setDoc(docRef, { counter: count });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+    };
+    fetchData();
+  }, [count]);
+
+  useEffect(() => {
+    const docRef = doc(firestore, "count", docId);
+    const unsubscribe = onSnapshot(docRef, (doc) => {
+      if (doc.exists()) {
+        setCount(doc.data().counter);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleIncrement = async () => {
+    const newCount = count + 1;
+    setCount(newCount);
+    try {
+      const docRef = doc(firestore, "count", docId);
+      await setDoc(docRef, { counter: newCount });
+    } catch (e) {
+      console.error("Error updating document: ", e);
+    }
+  };
+
   return (
-    <div className="flex">
-      <div className="flex gap-4">
-        <Button size="lg">heyy</Button>
-        <Label>heyy</Label>
+    <div className="flex justify-center">
+      <div className="flex gap-8 flex-col">
+        <Button size="lg" onClick={handleIncrement}>
+          press to increment
+        </Button>
+        <div className="flex text-center justify-center">
+          <Label>{count}</Label>
+        </div>
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-createRoot(document.getElementById('root')!).render(
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBXU4Jv-lSCp4IfeBPINGVmYJd3fs9ya5U",
+  authDomain: "fip-hifz.firebaseapp.com",
+  projectId: "fip-hifz",
+  storageBucket: "fip-hifz.firebasestorage.app",
+  messagingSenderId: "38455279748",
+  appId: "1:38455279748:web:2b3595c3409052f17882d1",
+  measurementId: "G-84RZXSQMN8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+export const firestore = getFirestore(app);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
