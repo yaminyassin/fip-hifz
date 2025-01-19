2a1aeef44342a7097b25d3e4dd1b6a8487cc8350
Yamin Yassin <yamin.yassin@millenniumbcp.pt>
2025-01-19 23:10:30 +0000

    feat: reworked models & updated queries

--- src/services/getCurrentParticipant.ts
+++ src/services/getCurrentParticipant.ts
@@ -2,27 +2,23 @@ import { firestore } from "@/main";
 import { Participant } from "@/models/models";
 import { collection, query, where, getDocs } from "firebase/firestore";
 
-export const getCurrentParticipantFromFirestore = async (): Promise<Participant | null> => {
-  try {
-    const participantsRef = collection(firestore, "participants");
-    const q = query(participantsRef, where("isActive", "==", true));
-    
-    const querySnapshot = await getDocs(q);
-    
-    if (querySnapshot.empty) {
-      return null;
+export const getCurrentParticipantFromFirestore =
+  async (): Promise<Participant | null> => {
+    try {
+      const participantsRef = collection(firestore, "participants");
+      const q = query(participantsRef, where("isActive", "==", true));
+
+      const querySnapshot = await getDocs(q);
+
+      if (querySnapshot.empty) {
+        return null;
+      }
+
+      // Assuming only one participant should be active at a time
+      const activeParticipant = querySnapshot.docs[0].data() as Participant;
+      return activeParticipant;
+    } catch (error) {
+      console.error("Error fetching active participant:", error);
+      throw error;
     }
-
-    // Assuming only one participant should be active at a time
-    const activeParticipant = querySnapshot.docs[0].data() as Participant;
-    return activeParticipant;
-  } catch (error) {
-    console.error("Error fetching active participant:", error);
-    throw error;
-  }
-};
-
-export const getCurrentParticipant = async () => {
-  const currentParticipant = await getCurrentParticipantFromFirestore();
-  return currentParticipant;
-};
+  };
